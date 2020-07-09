c19edc6b2415981fdc9c074f3d594c2fd459f062
sheikoodally <44279883+sheikoodally@users.noreply.github.com>
2020-07-09 12:18:15 -0400

    adding cart items

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
 
+import {cartItems} from './cart-items'
+
 function App() {
   return (
+
+    console.log(cartItems);
     <div className="App">
       <header className="App-header">
         
-          Learn React
       </header>
     </div>
   );
